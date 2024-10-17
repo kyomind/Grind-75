166e88eaece9f5d86c7230f6cf45478160040fbf
kyo <odinxp@gmail.com>
2024-10-17 14:19:29 +0800

    feat: implement Two Sum solution and clean up templates
    - Implement a new solution for the Two Sum problem
    - Remove the template index markdown file
    
    Signed-off-by: kyo <odinxp@gmail.com>

---
+++ solutions/1. Two Sum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        # 忘記把字典初始化放在迴圈外，我無言
+        hash_map = {}
+        for i, num in enumerate(nums):
+            if target - num in hash_map:
+                return [hash_map[target - num], i]
+            hash_map[num] = i
