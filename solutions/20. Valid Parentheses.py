1dc8575e8390153ca65d27a175678286e4a70b15
kyo <odinxp@gmail.com>
2024-11-27 12:27:41 +0800

    feat: 新增有效括號的解法，修正邏輯錯誤並優化程式碼實作

--- solutions/20. Valid Parentheses.py
+++ solutions/20. Valid Parentheses.py
@@ -1,5 +1,5 @@
 # 暴力法，但不可行，因為無法處理 "([)]" 這種情況
-class Solution(object):
+class Solution1:
     def isValid(self, s):
         """
         :type s: str
@@ -27,8 +27,9 @@ class Solution(object):
             return False
         return True
 
+
 # 使用 stack
-class Solution2(object):
+class Solution2:
     def isValid(self, s):
         """
         :type s: str
@@ -39,7 +40,8 @@ class Solution2(object):
             if c in ["(", "[", "{"]:
                 stack.append(c)
             else:
-                # 如果已經是空的，就不用再檢查了，不然會出現 IndexError
+                # 如果已經是空的，就不用再檢查了，不然 pop 下去會出現 IndexError
+                # XXX 2024-11-27 忘記這個確認
                 if not stack:
                     return False
                 if c == ")" and stack[-1] != "(":
@@ -50,7 +52,38 @@ class Solution2(object):
                     return False
                 stack.pop()
         # 不只過程中要檢查是否為空，全部結束後，也要檢查是否為空
+        # 以下寫法相當於 return not stack——但這種一行式的寫法不好懂
         if stack:
             return False
         return True
-        # 相當於 return not stack，但這種一行式的寫法不好懂
+
+
+# 2024-11-27
+class Solution:
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        for i in s:
+            if i in ["{", "(", "["]:
+                stack.append(i)
+            # 這裡可以確定，當前的 i 是結束符號，要從stack拉出一個對應的
+            # 而且要剛好對應——大對大、中對中、小對小，不可以對錯
+            else:
+                if not stack:
+                    return False
+                # 寫法不優雅，就是線性思考而已
+                if i == "}" and stack.pop() == "{":
+                    continue
+                if i == "]" and stack.pop() == "[":
+                    continue
+                if i == ")" and stack.pop() == "(":
+                    continue
+                else:
+                    return False
+
+        if stack:
+            return False
+        return True
