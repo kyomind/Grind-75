70f95a5bbac3fe806ff5f983d08c8c77c5fa1533
kyo <odinxp@gmail.com>
2024-12-01 11:22:31 +0800

    feat: 新增二元搜尋解法，包含詳細說明與程式碼實作，強調區間設置的選擇與影響

--- solutions/704. Binary Search.py
+++ solutions/704. Binary Search.py
@@ -1,6 +1,6 @@
 # 不可以遍歷，因為這樣的時間複雜度是O(n)，不符合題目要求
 # You must write an algorithm with O(log n) runtime complexity.
-class Solution(object):
+class Solution1:
     def search(self, nums, target):
         """
         :type nums: List[int]
@@ -13,10 +13,11 @@ class Solution(object):
 
         # 最多執行 len(nums) 次，但這裡不可能達到最大次數
         # 也可以用 while left <= right: 來寫
+        # XXX 2024-12-01 直接不管雙閉區間的 while 條件，狂了
         for _ in nums:
             # index 只能是整數，所以要用 // 來取整數
             # mid 是下一次的調整範圍邊界 index，取中間值
-            # 這個邊界值取到中間的偏左或右，都沒關係，因為接下來還會再比對範圍
+            # XXX 這個邊界值取到中間的偏左或右，都沒關係，因為接下來還會再比對範圍
             mid = (left + right) // 2
             # 剛好找到 target
             if nums[mid] == target:
@@ -28,4 +29,22 @@ class Solution(object):
             else:
                 right = mid - 1
 
+        return -1
+
+
+# 2024-12-01
+# 使用雙閉區間、while 迴圈
+class Solution:
+    def search(self, nums, target):
+        right = len(nums) - 1
+        left = 0
+        while right >= left:
+            mid = (right + left) // 2
+            if nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
+            # XXX 不能直接else，因為目標可能「不存在」
+            elif nums[mid] == target:
+                return mid
         return -1
