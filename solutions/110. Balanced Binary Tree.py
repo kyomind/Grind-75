de47d21f3d5129f283de2bf85b89657456f43a1e
kyo <odinxp@gmail.com>
2024-12-03 10:46:12 +0800

    feat: 新增「平衡二元樹」解法，包含詳細說明與程式碼實作，強調使用 helper 函式進行平衡性與高度計算

--- solutions/110. Balanced Binary Tree.py
+++ solutions/110. Balanced Binary Tree.py
@@ -1,12 +1,12 @@
 # Definition for a binary tree node.
-class TreeNode(object):
+class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
 
-class Solution(object):
+class Solution1:
     def isBalanced(self, root):
         """
         :type root: Optional[TreeNode]
@@ -42,3 +42,47 @@ class Solution(object):
             return max(left_height, right_height) + 1
 
         return check_height(root) != -1
+
+
+# 2024-12-03
+# 這次按照文件說的，helper函式回傳兩個值：是否平衡、高度
+# FIXME
+class Solution2:
+    def isBalanced(self, root):
+        # 遞迴、後序遍歷(先處理兩個子樹)
+        # return is_balanced, height
+        def helper(root):
+            # 停止條件: 先處理空節點或葉節點的子節點(空)
+            if root is None:
+                return True, 0
+
+            # 處理左右節點比較
+            # 有右節點(左右順序都可)
+            # FIXME 錯誤，因為當節點不為空時，就要「直接比較」子節點是否平衡了！
+            if root.left:
+                return helper(root.left)
+
+
+class Solution:
+    def isBalanced(self, root):
+        def helper(root):
+            # 停止條件: 先處理空節點或葉節點的子節點(空)
+            if root is None:
+                return True, 0
+
+            # 節點不為空，比較左右節點！
+            is_balanced_left, height_left = helper(root.left)
+            if not is_balanced_left:
+                return False, -1
+
+            is_balanced_right, height_right = helper(root.right)
+            if not is_balanced_right:
+                return False, -1
+
+            # 確定兩邊「子樹」都是平衡，繼續比較當前節點
+            if abs(height_right - height_left) > 1:
+                return False, -1
+
+            return True, max(height_left, height_right) + 1
+
+        return helper(root)[0]
