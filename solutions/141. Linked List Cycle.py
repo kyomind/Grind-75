cdab2ea80a4c97c7be9ca9a6db3e55e86f3da6f7
kyo <odinxp@gmail.com>
2024-12-04 09:21:10 +0800

    feat: 新增「鏈結串列循環」解法，包含雙指針與哈希表方法，修正錯誤示範並優化程式碼邏輯

--- solutions/141. Linked List Cycle.py
+++ solutions/141. Linked List Cycle.py
@@ -1,12 +1,12 @@
 # Definition for singly-linked list.
-class ListNode(object):
+class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
 
 
 # 雙指針法
-class Solution(object):
+class Solution1:
     def hasCycle(self, head):
         """
         :type head: ListNode
@@ -16,12 +16,13 @@ class Solution(object):
         # 起點相同，快指針走兩步，慢指針走一步
         fast = slow = head
 
-        # 當 head 不為 None 時，進入迴圈
+        # 當 head 不為 None 時，進入迴圈——XXX 無限迴圈
         # XXX 脫離迴圈的方式只有兩種：
         # 1. 快指針走到 linked list 的尾端
         # 2. 進入 cycle 且快慢指針相遇
         while head:
             # XXX 快指針經過的節點都不得為 None（這一步和下一步不得為 None）
+            # 2024-12-04 真的很重要！
             if fast and fast.next:
                 fast = fast.next.next
             # 快指針走到 None 時，代表已達 linked list 尾端，結束迴圈，無cycle
@@ -33,8 +34,6 @@ class Solution(object):
             slow = slow.next
 
             # 如果快慢指針相遇，代表有cycle
-            # 寫 is 是比較嚴謹的，雖然 == 也可以
-            # 因為 Python 預設 == 是比較記憶體位置，而我們也沒有重寫__eq__方法
             if fast is slow:
                 return True
 
@@ -43,3 +42,26 @@ class Solution(object):
 
 
 # hash table
+
+
+# 2024-12-04
+# 先用雙指針
+class Solution:
+    def hasCycle(self, head):
+        if head is None:
+            return False
+
+        fast = slow = head
+        # 如果用 head，可以直接省略上面的檢查，查優雅
+        while True:
+            slow = slow.next
+            if fast and fast.next:
+                fast = fast.next.next
+            else:
+                # 快指針先結束，確定沒有循環
+                # XXX 有循環的話，快慢指針都「永遠不會結束」
+                return False
+
+            if slow is fast:
+                # 只有一種情況為true，即兩個指針相遇
+                return True
