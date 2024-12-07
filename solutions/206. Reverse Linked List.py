2871db3fd4e909489540e329050f2d2af53f901f
kyo <odinxp@gmail.com>
2024-12-07 12:15:24 +0800

    feat: 新增「反轉鏈結串列」解法，詳細說明指針操作及初學者常見陷阱，並提供完整代碼示例

--- solutions/206. Reverse Linked List.py
+++ solutions/206. Reverse Linked List.py
@@ -1,5 +1,5 @@
 # Definition for singly-linked list.
-class ListNode(object):
+class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
@@ -15,12 +15,11 @@ ChatGPT 建議
 陣列存儲節點後指針方向未完全重新設置：在反轉鏈結串列時，應當斷開每個節點的原始指向，確保每個節點指向反轉過程中的新方向。
 錯誤的虛擬頭節點操作：最終返回 reversed_head.next 可能無法指向正確的反轉後的起點。
 """
-class Solution1(object):
+
+
+# 錯誤
+class Solution1:
     def reverseList(self, head):
-        """
-        :type head: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
         nodes = []
         # 虛擬頭節點，不是答案的一部分
         reversed_head = ListNode()
@@ -37,13 +36,10 @@ class Solution1(object):
 
         return reversed_head.next
 
+
 # 正確的做法(不考慮記憶體限制)
-class Solution1_1(object):
+class Solution2:
     def reverseList(self, head):
-        """
-        :type head: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
         # 1. 用陣列存儲所有節點的引用
         nodes = []
         while head:
@@ -60,14 +56,9 @@ class Solution1_1(object):
         return nodes[-1] if nodes else None
 
 
-
 # 與 ChatGPT 討論後重新來過，先用較簡單的迭代法
-class Solution(object):
+class Solution3:
     def reverseList(self, head):
-        """
-        :type head: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
         # 說真的，沒有這些自己的註解筆記，我很難看懂這段程式碼(一開始)
         prev = None
         current = head
@@ -79,10 +70,31 @@ class Solution(object):
             next_ = current.next
 
             current.next = prev  # XXX 這裡是關鍵，把當前節點的指針反轉
-            prev = current  # 更新「上一個節點」成為當前節點
+            prev = current  # 讓「上一個節點」更新為當前節點
 
             # 往下一個節點走，其實就是 current = current.next
             current = next_
 
         # 結束時，prev 就是反轉後的串列的頭節點(當前節點)
         return prev
+
+
+# 2024-12-07
+class Solution:
+    def reverseList(self, head):
+        prev = None
+        current = head
+        next_ = None
+        # while head 也不是不行，因為後不會再用到了
+        # 只是操作過程中，head 這變數名稱具有誤導性
+        while current:
+            # 下一個節點先存起來，不然之後就訪問不到了
+            next_ = current.next
+            current.next = prev
+            prev = current
+            current = next_
+
+        return prev
+
+
+# 下次優先徒手做出迴圈法就好，這就是標準操作，解法都一樣
