795ebaa70428766af4a91a153f65fcaf1b26091a
kyo <odinxp@gmail.com>
2024-12-08 12:25:48 +0800

    feat: 新增「二元樹最大深度」解法，包含基本 DFS 實作與關鍵註解說明

--- solutions/104. Maximum Depth of Binary Tree.py
+++ solutions/104. Maximum Depth of Binary Tree.py
@@ -1,5 +1,5 @@
 # Definition for a binary tree node.
-class TreeNode(object):
+class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
@@ -7,16 +7,13 @@ class TreeNode(object):
 
 
 # 這基本上就是標準的 DFS，後序遍歷，可以說要牢記於心
-class Solution(object):
+class Solution:
     def maxDepth(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: int
-        """
         if not root:
             return 0
 
         left = self.maxDepth(root.left)
         right = self.maxDepth(root.right)
 
+        # 這個是關鍵 2024-12-08
         return max(left, right) + 1
