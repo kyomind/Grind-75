3ef47ecec06f2d66d7ac14e20e22aee7bc7bd98d
kyo <odinxp@gmail.com>
2024-12-08 12:27:14 +0800

    fix: 更新註解以增強程式可讀性，明確說明停止條件及遞迴邏輯

--- solutions/104. Maximum Depth of Binary Tree.py
+++ solutions/104. Maximum Depth of Binary Tree.py
@@ -7,13 +7,16 @@ class TreeNode:
 
 
 # 這基本上就是標準的 DFS，後序遍歷，可以說要牢記於心
+# + 2024-12-08
 class Solution:
     def maxDepth(self, root):
+        # 一定要有停止條件 2024-12-08
         if not root:
             return 0
 
+        # 不是空節點就直接遞迴計算左右節點高度
         left = self.maxDepth(root.left)
         right = self.maxDepth(root.right)
 
-        # 這個是關鍵 2024-12-08
+        # 這個是關鍵，最後回傳並增加高度
         return max(left, right) + 1
