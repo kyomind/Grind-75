b4d2ccb8aa4fb0233f55aa522fda32d3d783b473
kyo <odinxp@gmail.com>
2024-12-09 13:15:42 +0800

    feat: 新增「插入區間」解法，包含詳細步驟與常見錯誤解析

--- solutions/57. Insert Interval.py
+++ solutions/57. Insert Interval.py
@@ -4,10 +4,11 @@
 一、初始化一個空的結果陣列 new_intervals（題目允許另建一個陣列，不必原地修改）
 
 二、遍歷 intervals 中的每個 interval，分成三種情況處理：
-- 若 interval 在 newInterval 的左側（即 interval[1] < newInterval[0]），將 interval 加入 new_intervals
-    因為已經確保不會有重疊
+- 若 interval 在 newInterval 的左側（即 interval[1] < newInterval[0]）
+    將 interval 加入 new_intervals，因為已經確保不會有重疊
 - 若 interval 在 newInterval 的右側（即 interval[0] > newInterval[1]）
-    將 newInterval 加入 new_intervals，並將剩餘的區間也全部加入 new_intervals，因為已經確保不會有重疊
+    將 newInterval 加入 new_intervals，並將剩餘的區間也全部加入 new_intervals
+    因為已經確保不會有重疊
     但也可以更新 newInterval = interval，這樣下一輪就可以處理 interval，直到迴圈結束(比較沒有效率)
 - 若 interval 與 newInterval 有重疊，則更新 newInterval 的範圍，使之包含 interval
 
@@ -15,13 +16,8 @@
 """
 
 
-class Solution1(object):
+class Solution1:
     def insert(self, intervals, newInterval):
-        """
-        :type intervals: List[List[int]]
-        :type newInterval: List[int]
-        :rtype: List[List[int]]
-        """
         new_intervals = []
         for i in intervals:
             if i[1] < newInterval[0]:  # 完全在 newInterval 左側
@@ -44,7 +40,7 @@ class Solution1(object):
 # - 好吧，我承認，這樣寫並沒有更好懂
 # - 反而上面那個更直觀、無腦
 # 使用 enumerate 可以得到 index，這樣就可以直接切片加入剩餘的區間
-class Solution(object):
+class Solution2:
     def insert(self, intervals, newInterval):
         if not intervals:
             return [newInterval]
