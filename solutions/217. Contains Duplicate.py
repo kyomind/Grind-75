b144c235333bd7f49b0d184713438787697edbf0
kyo <odinxp@gmail.com>
2024-12-09 12:29:55 +0800

    feat: 新增「包含重複元素」解法，包含多種實作方式及詳細註解

--- solutions/217. Contains Duplicate.py
+++ solutions/217. Contains Duplicate.py
@@ -5,12 +5,8 @@
 
 
 # hash table
-class Solution1(object):
+class Solution1:
     def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
         counter = {}
         for i in nums:
             # 對耶，完全不用管值，只要 key 重複了就是有重複的元素
@@ -23,32 +19,36 @@ class Solution1(object):
         return False
 
 
-# 犯規做法，使用Python set
+# 犯規做法，直接使用 Python set 的特性
 # 僅供參考，不建議這樣寫
 # beat 99%
-class Solution2(object):
+class Solution2:
     def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
         return len(nums) != len(set(nums))
 
 
 # 其實有兩種的融合，直接使用set來實作原來的hash table
 # 這樣就不用考慮值的問題，更加簡潔
 # 因為用了迴圈，所以還是沒有直接使用set快(beat 72%)
-class Solution(object):
+class Solution3:
     def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
         counter = set()
         for i in nums:
             if i in counter:
                 return True
 
+            counter.add(i)
+
+        return False
+
+
+# 2024-12-09
+class Solution:
+    def containsDuplicate(self, nums):
+        counter = set()
+        for i in nums:
+            if i in counter:
+                return True
             counter.add(i)
 
         return False
