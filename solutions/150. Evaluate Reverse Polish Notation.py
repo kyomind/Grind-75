09df2db8106ad6b49fa3ae2251c34edfc363c78a
kyo <odinxp@gmail.com>
2024-12-18 12:44:03 +0800

    feat: 新增逆波蘭表示法的詳細說明與堆疊使用的必要性分析

--- solutions/150. Evaluate Reverse Polish Notation.py
+++ solutions/150. Evaluate Reverse Polish Notation.py
@@ -5,10 +5,9 @@ class Solution:
 
         # 遍歷所有的 token
         for token in tokens:
-            # 如果 token 是運算符
-            if token in {"+", "-", "*", "/"}:
-                # 從堆疊中彈出兩個操作數
-                b = stack.pop()  # XXX 注意：基於 stack 特性，先彈出的是第二個操作數
+            if token in {"+", "-", "*", "/"}:  # token 為運算符
+                # 從堆疊中彈出兩個操作數字
+                b = stack.pop()  # XXX 注意：基於 stack 特性，先彈出的是第二個操作數，這是一大重點！
                 a = stack.pop()  # 後彈出的是第一個操作數
 
                 # 根據不同的運算符進行計算
@@ -26,9 +25,8 @@ class Solution:
                 # 將計算結果壓回堆疊
                 stack.append(result)
 
-            else:
-                # 如果 token 是數字，則轉換為整數後壓入堆疊
-                stack.append(int(token))
+            else:  # token 為數字
+                stack.append(token)
 
         # 堆疊中最後剩下的唯一元素即為計算結果
         return stack[0]
