a43acd2af12eedb9f348f5a411f1a0fae9d8a81b
kyo <odinxp@gmail.com>
2025-02-02 13:06:52 +0800

    feat: 更新「無重複字元的最長子串」解法，增強註解以說明滑動視窗中重複字元的處理邏輯

--- solutions/3. Longest Substring Without Repeating Characters.py
+++ solutions/3. Longest Substring Without Repeating Characters.py
@@ -2,7 +2,7 @@
 # 可以用字典或集合來記錄當前子串中的字元
 
 
-class Solution:
+class Solution3:
     def lengthOfLongestSubstring(self, s):
         """
         字典法→更加優雅，但是理解的負擔更大(較為抽象)
@@ -29,7 +29,7 @@ class Solution:
         return max_length
 
 
-class Solution1:
+class Solution11:
     def lengthOfLongestSubstring(self, s):
         """
         集合法→更基本的做法
@@ -45,6 +45,14 @@ class Solution1:
             # 如果字元已經出現過，則更新起始位置
             # 如果重複字元在當前子字串的中段或後段，需要多次移動 left 指針，所以會用 while 迴圈
             while char in char_set:
+                """
+                這裡的一大重點是，當char已經在set中時，那就要讓當前這個char去取代過去的相同值
+                此時要移動left指針，如果移動一次，set中的char就沒了
+                - 那就可以繼續把當前的加進來，此時的差別是left指針移動了
+                但如果一次還不夠，就是移動多次，但最終當前的char還是會加進來set
+                - 移動多次並不是因為有多個重複(事實上只會有一個重複)
+                - 而是移動一次，還無法消除重複，而需要移動多次
+                """
                 # XXX 必須移除元素，不然 while 是「無限」迴圈
                 # 雖然脫離迴圈後，又立刻加回集合中了，但先脫離是必要條件
                 char_set.remove(s[left])
@@ -53,3 +61,9 @@ class Solution1:
             char_set.add(char)  # 又加回來了，但這次是因為最新的字元
             max_length = max(max_length, right - left + 1)
         return max_length
+
+
+# 2025-02-02
+# 不要用 enumerate
+class Solution:
+    def lengthOfLongestSubstring(self, s: str): ...
