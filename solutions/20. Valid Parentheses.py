c9efaf1e1de90c5620089415c5e7b479178da8b7
kyo <odinxp@gmail.com>
2025-02-06 12:28:33 +0800

    修正「有效括號」解法中的堆疊檢查邏輯，增強對空堆疊存取的處理，並優化字典鍵值設計以提高查詢效率

--- solutions/20. Valid Parentheses.py
+++ solutions/20. Valid Parentheses.py
@@ -59,7 +59,7 @@ class Solution2:
 
 
 # 2024-11-27
-class Solution:
+class Solution22:
     def isValid(self, s):
         """
         :type s: str
@@ -72,7 +72,7 @@ class Solution:
             # 這裡可以確定，當前的 i 是結束符號，要從stack拉出一個對應的
             # 而且要剛好對應——大對大、中對中、小對小，不可以對錯
             else:
-                if not stack:
+                if not stack: # XXX 太容易忘記檢查
                     return False
                 # 寫法不優雅，就是線性思考而已
                 if i == "}" and stack.pop() == "{":
@@ -87,3 +87,29 @@ class Solution:
         if stack:
             return False
         return True
+
+
+class Solution:
+    def isValid(self, s):
+        # XXX pairs ={'{':'}','(':')','[':']'}
+        # 必須是 key 為下括號，因為key→value非常直接，從value到key很麻煩
+        # 而pairs[i]我需要得到的是上括號！故設為value
+        pairs = {"}": "{", ")": "(", "]": "["}
+        stack =[]
+        for i in s:
+            if i in pairs.values():
+                stack.append(i)
+
+            else:
+                # XXX if stack.pop() == pairs[i]: 這樣寫一定會 keyError
+                # 因此此時的i必定是}或)或]，不要搞笑
+                # XXX if not stack.pop() == pairs[i]: 第二個錯誤，條件還不夠
+                # 要檢查 stack 是否為空，因為不合法的資料會讓 stack 為空
+                if not stack or stack.pop() != pairs[i]:
+                    return False
+
+        return len(stack)==0
+
+"""
+else 和 if 可以進一步整合為 elif 就好了，else 顯得有點多餘
+"""
