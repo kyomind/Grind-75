caa2aee725b5d7596c667d7dfc5816477bc942ef
kyo <odinxp@gmail.com>
2025-02-07 11:28:31 +0800

    修正反轉二元樹的解法，刪除誤導性內容，調整程式碼邏輯以符合標準遞迴結構，增強可讀性與註解清晰度

--- solutions/226. Invert Binary Tree.py
+++ solutions/226. Invert Binary Tree.py
@@ -6,35 +6,9 @@ class TreeNode:
         self.right = right
 
 
-# 使用遞迴解法(適合初學者)
-class Solution1:
+# 2024-11-30 遞迴
+class Solution2:
     def invertTree(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: Optional[TreeNode]
-        """
-        # 設定停止條件
-        # 2024-11-30 遇到葉節點或root是空的時有效
-        if root is None:
-            return None
-
-        # 交換左右子樹
-        root.left, root.right = root.right, root.left
-
-        # 遞迴處理左右子樹
-        self.invertTree(root.left)
-        self.invertTree(root.right)
-
-        return root
-
-
-# 2024-11-30 遞迴 + 後序遍歷
-class Solution:
-    def invertTree(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: Optional[TreeNode]
-        """
         if root is None:
             return None
 
@@ -42,5 +16,25 @@ class Solution:
         # 先左或右都可以
         self.invertTree(root.right)
         self.invertTree(root.left)
+
+        return root
+
+
+# 2025-02-07
+# XXX 重點是：先處理當前節點，再處理左右子節點，邏輯都一樣，所以使用遞迴
+# 這算是一種「前序遍歷」
+class Solution:
+    def invertTree(self, root):
+        if root is None:
+            return None
+
+        # 先翻轉(處理)右左子樹, 這是處理 XXX 「當前節點」
+        # root.left = root.right XXX 這樣寫是不行的！只能寫在一行，不然就要「中間變數」來交換
+        # root.right = root.left
+        root.left, root.right = root.right, root.left
+
+        # 遞迴處理 XXX 「子節點」的左右子樹
+        self.invertTree(root.left)
+        self.invertTree(root.right)
 
         return root
