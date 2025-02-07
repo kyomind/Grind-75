6698dc7b6dfc4948ec9e8cd0e793c216cb8ac1b9
kyo <odinxp@gmail.com>
2025-02-07 18:50:42 +0800

    修正「兩數之和」解法，調整哈希表更新邏輯以避免誤判，增強註解清晰度並強調正確的操作步驟

--- solutions/1. Two Sum.py
+++ solutions/1. Two Sum.py
@@ -19,7 +19,33 @@ class Solution2:
         hash_map = {}
         for i, n in enumerate(nums):
             # 為何這裡只是提前，就無法通過了？詳見Markdown
-            hash_map[n] = i
+            # 2025-02-07 其實你可以用 if/else 就沒這問題了
+            # hash_map[n] = i XXX 位置錯誤
             # XXX 這行是最重要的，完全解釋了為何 key 是值而不是索引
             if target - n in hash_map:
-                return [hash_map[n], i]
+                # return [hash_map[n], i]  # XXX 錯誤了
+                return [hash_map[target - n], i]
+
+            hash_map[n] = i
+
+
+"""
+一個簡單的道理就是你把 If/Else 改成 衛語句 的時候 If 一定是先的
+所以 hash_map[n] = i 不可以放在 if 之前
+"""
+
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        num_to_index_mapping = {}
+        for i in range(len(nums)):
+            # target - nums[i]，就是key
+            # another_num = target - nums[i]
+            # if another_num in num_to_index_mapping:
+            if target - nums[i] in num_to_index_mapping:
+                # 這裡只是使用同一個 key 去取值而已！完全沒變
+                # return [i, num_to_index_mapping[another_num]]
+                return [i, num_to_index_mapping[target - nums[i]]]
+
+            else:
+                num_to_index_mapping[nums[i]] = i
