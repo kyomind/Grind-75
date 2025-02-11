52855b6408233caf0037722f8f0559a6b469f918
kyo <odinxp@gmail.com>
2025-02-11 18:19:06 +0800

    新增「最小堆疊」解法，採用兩個堆疊同步的方式實現，並增強註解以清晰說明邏輯與使用場景

--- solutions/155. Min Stack.py
+++ solutions/155. Min Stack.py
@@ -1,4 +1,4 @@
-class MinStack:
+class MinStack1:
     def __init__(self):
         # 主堆疊，用來儲存所有插入的元素
         self.stack = []
@@ -29,16 +29,8 @@ class MinStack:
         # 直接返回輔助堆疊的頂端元素，即當前的最小值
         return self.min_stack[-1]
 
-
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(val)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
-
 """
-兩個重要的問題與解答
+兩個重要的問題與解答(這兩個問題都是兩個堆疊「不同步」才有的議題)
 
 一、為什麼「等於」也要插入？而不是只有小於才插入輔助stack
 「等於」的情況下也插入輔助堆疊的原因是為了處理重複的最小值
@@ -48,3 +40,35 @@ class MinStack:
 當一個元素從主堆疊彈出時，如果該元素不是最小值，輔助堆疊不會發生任何變化
 只有當彈出的元素是當前的最小值時，輔助堆疊才會同步彈出該值
 """
+
+# 2025-02-11 採用兩個堆疊同步的寫法
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        # 可以再化簡，但我覺得不化簡也沒關係
+        if self.min_stack:
+            min_stack_top = self.min_stack[-1]
+            if val < min_stack_top:
+                self.min_stack.append(val)
+            else:
+                self.min_stack.append(min_stack_top)
+        else:
+            self.min_stack.append(val)
+
+
+    def pop(self) -> None:
+        if self.stack:
+            self.stack.pop()
+            self.min_stack.pop()
+
+    def top(self) -> int:
+        # 這可以通過，顯然測資不會在空堆疊時嘗試top
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        # 這可以通過，顯然測資不會在空堆疊時嘗試getMin
+        return self.min_stack[-1]
