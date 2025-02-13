e8f54af7c7d5044a7b6ec6644ec831f3054b5793
kyo <odinxp@gmail.com>
2025-02-13 11:38:15 +0800

    增強「旋轉排序陣列中的最小值」解法的註解，清晰說明二分搜尋法中的邏輯與分界點判斷

--- solutions/153. Find Minimum in Rotated Sorted Array.py
+++ solutions/153. Find Minimum in Rotated Sorted Array.py
@@ -5,10 +5,13 @@ class Solution:
         while left < right:
             mid = (left + right) // 2
 
+            # 答案在不正常有序的那側，即分界點
             if nums[mid] > nums[right]:
+                # 右半邊不正常，答案在右半邊
                 left = mid + 1
 
             else:
+                # 答案在左半邊，且包括mid本身也可能是答案
                 right = mid
 
         # 兩者最終會相遇，因為並不會跳著移動
