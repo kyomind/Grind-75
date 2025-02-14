c691ced6a2a880d84ea544e81070983172800161
kyo <odinxp@gmail.com>
2025-02-14 09:23:17 +0800

    增強「旋轉排序陣列中的最小值」解法的註解，清晰說明終止條件與指針移動邏輯

--- solutions/153. Find Minimum in Rotated Sorted Array.py
+++ solutions/153. Find Minimum in Rotated Sorted Array.py
@@ -2,6 +2,7 @@ class Solution:
     def findMin(self, nums: list[int]) -> int:
         left = 0
         right = len(nums) - 1
+        # # 搜尋範圍是 [left, right)，因此終止條件是 left == right
         while left < right:
             mid = (left + right) // 2
 
@@ -14,5 +15,6 @@ class Solution:
                 # 答案在左半邊，且包括mid本身也可能是答案
                 right = mid
 
-        # 兩者最終會相遇，因為並不會跳著移動
+        # 兩者最終會相遇，達成終止條件，脫離迴圈
+        # 因為指針並不會跳著移動(+2/-2)，所以相遇時就會脫離迴圈，不可能發生 left > right 的情況
         return nums[left]
